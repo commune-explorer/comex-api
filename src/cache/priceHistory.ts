e59d2b06f6701f14c07f7444b0581b63932fa204
Supremesource <zimulajan0@gmail.com>
2024-06-20 12:26:09 +0200

    adding longer price query

--- src/cache/priceHistory.ts
+++ src/cache/priceHistory.ts
@@ -1,26 +1,36 @@
-import { Cache } from './basic/base'
-import axios from 'axios'
-import { PriceRecord } from '../models/priceRecord'
-import { RedisKey } from '../constants/common'
+import { Cache } from './basic/base';
+import axios, { AxiosResponse } from 'axios';
+import { PriceRecord } from '../models/priceRecord';
+import { RedisKey } from '../constants/common';
 
 export class PriceHistoryCache extends Cache<PriceRecord[]> {
-  public intervalSeconds = 4 * 60 * 60
-  public cacheKey: RedisKey = 'price_history'
+  public intervalSeconds = 4 * 60 * 60;
+  public cacheKey: RedisKey = 'price_history';
 
   public async fetch() {
-    console.log('start fetch price history...')
-    const {
-      data: {
-        data: { points },
-      },
-    } = await axios.get('https://api.coinmarketcap.com/data-api/v3/cryptocurrency/detail/chart?id=29509&range=ALL')
+    console.log('Start fetch price history...');
 
-    return Object.entries(points)
-      .map(([timestamp, item]: any) => ({
-        timestamp,
-        price: item.v[0],
-        volume: item.v[1],
-      }))
-      .sort((a, b) => a.timestamp - b.timestamp)
+    try {
+      const response: AxiosResponse = await axios.get('https://api.coingecko.com/api/v3/coins/commune-ai/market_chart', {
+        params: {
+          vs_currency: 'usd',    
+          days: '365'    
+        }
+      });
+
+      const { prices, total_volumes } = response.data;
+
+      const points: PriceRecord[] = prices.map((price: [number, number], index: number) => ({
+        timestamp: price[0],
+        price: price[1],
+        volume: total_volumes[index] ? total_volumes[index][1] : 0 // Default volume to 0 if null
+      }));
+
+      return points.sort((a: PriceRecord, b: PriceRecord) => a.timestamp - b.timestamp);
+
+    } catch (error) {
+      console.error('Error fetching price history:', error);
+      throw error;
+    }
   }
 }
