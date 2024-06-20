4a737e34681a468417e24b2b1187dfde4d05b4dc
Honza <115138297+Supremesource@users.noreply.github.com>
2024-06-20 11:48:11 +0200

    Update subnet.ts

--- src/constants/subnet.ts
+++ src/constants/subnet.ts
@@ -1,8 +1,9 @@
 export const SUBNET_REPO: { [key: number]: string } = {
   0: 'https://github.com/commune-ai/commune/',
+  1: 'https://github.com/nakamoto-ai/zangief',
+  2: 'https://github.com/smart-window/comchat-subnet',
   3: 'https://github.com/agicommies/synthia/',
   5: 'https://github.com/Open0xScope/openscope/',
-  6: 'https://github.com/smart-window/comchat-subnet',
   10: 'https://github.com/Agent-Artificial/eden-subnet/',
   14: 'https://github.com/mosaicx-org/mosaic-subnet/',
   17: 'https://github.com/MarketCompassDev/marketcompass-subnet/',
