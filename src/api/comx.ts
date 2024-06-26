468a12f31f9233704109e6d47de60a2a7462fc26
callmewhy <whywanghai@gmail.com>
2024-06-26 21:13:43 +0800

    format code

--- src/api/comx.ts
+++ src/api/comx.ts
@@ -23,41 +23,40 @@ export async function fetchSubnetModules(netuid: number) {
   return modules
 }
 
-
 export async function fetchDailyEmission() {
   try {
-    console.info('Fetching daily emission from:', options.baseURL);
+    console.info('Fetching daily emission from:', options.baseURL)
     const {
       data: { daily_emission },
-    } = await axios.get<{ daily_emission: number }>('/daily-emission', options);
-    console.info('fetchDailyEmission:', daily_emission);
-    return daily_emission;
+    } = await axios.get<{ daily_emission: number }>('/daily-emission', options)
+    console.info('fetchDailyEmission:', daily_emission)
+    return daily_emission
   } catch (error: any) {
     if (axios.isAxiosError(error)) {
-      console.error('Axios error message:', error.message);
-      console.error('Axios error response:', error.response?.status, error.response?.data);
+      console.error('Axios error message:', error.message)
+      console.error('Axios error response:', error.response?.status, error.response?.data)
     } else {
-      console.error('Unexpected error:', error);
+      console.error('Unexpected error:', error)
     }
-    throw error;
+    throw error
   }
 }
 
 export async function fetchValidatingApr() {
   try {
-    console.info('Fetching daily apr from:', options.baseURL);
+    console.info('Fetching daily apr from:', options.baseURL)
     const {
       data: { apr },
-    } = await axios.get<{ apr: number }>('/apr', options);
-    console.info('fetchDailyApr:', apr);
-    return apr;
+    } = await axios.get<{ apr: number }>('/apr', options)
+    console.info('fetchDailyApr:', apr)
+    return apr
   } catch (error: any) {
     if (axios.isAxiosError(error)) {
-      console.error('Axios error message:', error.message);
-      console.error('Axios error response:', error.response?.status, error.response?.data);
+      console.error('Axios error message:', error.message)
+      console.error('Axios error response:', error.response?.status, error.response?.data)
     } else {
-      console.error('Unexpected error:', error);
+      console.error('Unexpected error:', error)
     }
-    throw error;
+    throw error
   }
 }
--- src/cache/priceHistory.ts
+++ src/cache/priceHistory.ts
@@ -1,33 +1,36 @@
-import { Cache } from './basic/base';
-import axios, { AxiosResponse } from 'axios';
-import { PriceRecord } from '../models/priceRecord';
-import { RedisKey } from '../constants/common';
+import { Cache } from './basic/base'
+import axios, { AxiosResponse } from 'axios'
+import { PriceRecord } from '../models/priceRecord'
+import { RedisKey } from '../constants/common'
 import * as fs from 'node:fs'
 
 const comswapData = JSON.parse(fs.readFileSync('comswap.json', 'utf8')) as any[]
 
 export class PriceHistoryCache extends Cache<PriceRecord[]> {
-  public intervalSeconds = 4 * 60 * 60;
-  public cacheKey: RedisKey = 'price_history';
+  public intervalSeconds = 4 * 60 * 60
+  public cacheKey: RedisKey = 'price_history'
 
   public async fetch() {
-    console.log('Start fetch price history...');
+    console.log('Start fetch price history...')
 
     try {
-      const response: AxiosResponse = await axios.get('https://api.coingecko.com/api/v3/coins/commune-ai/market_chart', {
-        params: {
-          vs_currency: 'usd',    
-          days: '365'    
+      const response: AxiosResponse = await axios.get(
+        'https://api.coingecko.com/api/v3/coins/commune-ai/market_chart',
+        {
+          params: {
+            vs_currency: 'usd',
+            days: '365',
+          },
         }
-      });
+      )
 
-      const { prices, total_volumes } = response.data;
+      const { prices, total_volumes } = response.data
 
       const points: PriceRecord[] = prices.map((price: [number, number], index: number) => ({
-        timestamp: Math.floor(price[0] / 1000),  // Convert milliseconds to seconds
+        timestamp: Math.floor(price[0] / 1000), // Convert milliseconds to seconds
         price: price[1],
-        volume: total_volumes[index] ? total_volumes[index][1] : 0  // Default volume to 0 if null
-      }));
+        volume: total_volumes[index] ? total_volumes[index][1] : 0, // Default volume to 0 if null
+      }))
 
       // load comswap data at the beginning
       const startTime = Math.min(...points.map((i: PriceRecord) => i.timestamp))
@@ -41,8 +44,8 @@ export class PriceHistoryCache extends Cache<PriceRecord[]> {
 
       return [...oldPoints, ...points].sort((a: PriceRecord, b: PriceRecord) => a.timestamp - b.timestamp)
     } catch (error) {
-      console.error('Error fetching price history:', error);
-      throw error;
+      console.error('Error fetching price history:', error)
+      throw error
     }
   }
 }
